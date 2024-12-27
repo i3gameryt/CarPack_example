82dcc133046da32dfc17220616fd6fcb5b84a238
i3gameryt <rajavarapuakash2007@gmail.com>
2024-12-27 07:56:51 +0530

    i3
    fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Your Name'
+description 'Combined Addon Cars Resource'
+version '1.0.0'
+
+files {
+    'data/**/vehicles.meta',
+    'data/**/carvariations.meta',
+    'data/**/carcols.meta',
+    'data/**/handling.meta',
+    'data/**/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
